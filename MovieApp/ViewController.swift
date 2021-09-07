22127d5f0314750ce474d9e1becd591ed3c20caf
qadriyyah119 <qmadinah@gmail.com>
2021-09-07 13:45:31 -0400

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Qadriyyah Thomas on 9/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
